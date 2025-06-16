9c5f693013d7c632a4ecbc3f975c1dde8155175e
JuliyaPershina <jpershina79@gmail.com>
2025-06-16 22:00:03 +0300

    Розібратися з Clearall  та округленням

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const wrappers = document.querySelectorAll('.input-wrapper');
+// const amount = +document.getElementById('amount').value;
+
+// const termYears = +document.getElementById('term').value;
+// const annualRate = +document.getElementById('rate').value;
 
 //підсвітка полів input
 wrappers.forEach((wrapper) => {
@@ -36,3 +40,62 @@ wrappers.forEach((wrapper) => {
     return input.value.trim() === '';
   }
 });
+
+// Розрахунок Repayment
+
+function calculateRepayment(amount, termYears, annualRate) {
+  const n = termYears * 12;
+  const r = annualRate / 100 / 12;
+  const numerator = amount * r * Math.pow(1 + r, n);
+  const denominator = Math.pow(1 + r, n) - 1;
+  const monthlyPayment = numerator / denominator;
+  const totalRepayment = monthlyPayment * n;
+  return {
+    monthlyPayment: +monthlyPayment.toFixed(2),
+    totalPaid: +totalRepayment.toFixed(2),
+  };
+}
+
+// Розрахунок Interest Only
+
+function calculateInterestOnly(amount, termYears, annualRate) {
+  const n = termYears * 12;
+  const r = annualRate / 100 / 12;
+  const monthlyPayment = amount * r;
+  const totalPaid = monthlyPayment * n;
+  return {
+    monthlyPayment: +monthlyPayment.toFixed(2),
+    totalPaid: +totalPaid.toFixed(2),
+  };
+}
+// Обираємо тип розрахунку
+
+function checkMortageType() {
+  const calculateButton = document.querySelector('.submit');
+  calculateButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    const mortageType = document.querySelector(
+      'input[type="radio"]:checked'
+    ).id;
+    const amount = +document.getElementById('amount').value;
+
+    const termYears = +document.getElementById('term').value;
+    const annualRate = +document.getElementById('rate').value;
+
+    let calculations;
+
+    if (mortageType === 'repayment') {
+      calculations = calculateRepayment(amount, termYears, annualRate);
+    } else {
+      calculations = calculateInterestOnly(amount, termYears, annualRate);
+    }
+    document.getElementById(
+      'monthly-repayment'
+    ).textContent = `£${calculations.monthlyPayment}`;
+    document.getElementById(
+      'total-repayment'
+    ).textContent = `£${calculations.totalPaid}`;
+    console.log(calculations.monthlyPayment, calculations.totalPaid);
+  });
+}
+checkMortageType();
